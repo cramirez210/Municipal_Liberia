3319af48796fb0884e6db7e102ace04d391c986a
Josue-Barrantes <josuebs12@gmail.com>
2017-11-29 12:31:44 -0600

    ..

--- resources/views/reportes/tiposReportes/reporteFactura.blade.php
+++ resources/views/reportes/tiposReportes/reporteFactura.blade.php
@@ -8,6 +8,11 @@
 
 @section('contenido')
 
-@include('reportes.tiposReportes.machoteFactura')
+<div class="py-4">
+	@include('reportes.tiposReportes.machoteFactura')
+</div>
 
+<div class="print"> 
+    @include('reportes.imprimir')
+</div>
 @endsection
