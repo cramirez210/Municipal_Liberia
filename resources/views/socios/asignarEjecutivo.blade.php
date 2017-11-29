f7942f4d268cf4907351e5b23bca700e27b2bb09
Ramirez PC <c.and.95@outlook.es>
2017-11-29 11:31:28 -0600

    Regresar

--- resources/views/socios/asignarEjecutivo.blade.php
+++ resources/views/socios/asignarEjecutivo.blade.php
@@ -2,66 +2,43 @@
 
 @section('content')
 
-<!-- <div class="card text-center mt-4 py-5">
-  <div class="card-header">
-    
-    <label class="text-primary"> Filtro de Busqueda </label>
-    <div class="nav-item float-right">
-       <a class="btn btn-warning btn-xs fa fa-exclamation-triangle system-icons" href="/socios/index" ">Regresar</a>
-    </div>
-  </div>
-
-  <div class="card-block">
-
-  <form>
-  
-  <select id="select" class="custom-select mb-2 mr-sm-2 mb-sm-0" name="criterio">
-    <option selected value="0">Nombre de Usuario</option>
-    <option value="1">Nombre o Apellido</option>
-  </select>
-
-  <label class="custom-control custom-checkbox mb-2 mr-sm-2 mb-sm-0">
-        <input id="valor" type="text" class="form-control" placeholder="Ejemplo: Usuario" type="text" name="valor" value="{{ old('valor') }}" required autofocus onkeyup="filtrar()">
-  </label>
-  </form> 
-
-</div>
-
-</div>
-
-@if(count($errors))
-<div >
-<br>
-  <span class="text-danger mt-4">
-        @if($errors->has('radio'))
-          <span class="form-control-feedback">
-            <strong>{{ $errors->first('radio') }}</strong>
-          </span>
-        @endif
-  </span>
-</div>
-@endif -->
-
-
 @include('mensajes.alertas')
 
-<button class="btn btn-success btn-md mt-3 fa fa-search system-icons" data-toggle="modal" data-target="#filtrar_users"> Realizar una búsqueda</button>
+<button class="btn btn-success btn-md mt-3 fa fa-search system-icons mb-2" data-toggle="modal" data-target="#filtrar_users"> Realizar una búsqueda</button>
 @include('socios.filtrarEjecutivo')
 
 <!--_______________________________ Tabla _____________________________-->
 <form method="GET" action="/socios/home" >
   {{ csrf_field() }}
-<div class="card text-center mt-4">
+<div class="card text-center ">
+
+
+
+
   <div class="card-header">
-     <label class="text-primary"> Ejecutivos Registrados </label>
-    <div class="nav-item float-right">
-       <button type="submit" class="btn btn-success btn-xs" style="color: white;">Siguiente</button>
-    </div>
+
+    <div class="row ">
+      <div class="col-md-7 text-md-right text-xs-center">
+             <label class="text-primary"> Ejecutivos Registrados </label>      
+      </div>
+
+       <div class="col-md-5">
+           <div class="nav-item float-md-right float-xs-center">
+       <button type="submit" class="btn btn-success btn-xs fa fa-check system-icons" style="color: white;">Siguiente</button>
+         <a href="/socios/index" class="btn btn-danger fa fa-times system-icons">
+        <span class="glyphicon glyphicon-remove-circle "></span> Cancelar </a>
+    </div>  
+      </div>
+  
+    
+    
+      </div>
+         
   </div>
-  <div class="card-body tab-content">
+  <div class="card-body tab-content mt-md-4">
     <div class="tab-pane active" id="tabc" role="tabpanel">
     
-    <div class="container-fluid col-md-9">
+    <div class="container col-md-8">
 <div class="row">
 
  <div class="table-responsive  table-condensed">
