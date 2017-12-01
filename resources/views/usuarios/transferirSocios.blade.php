b931c49613a5458bd8bc3800ebada1881e7b8dca
jafet chevez <jafet.chevez14@gmail.com>
2017-11-30 21:17:43 -0600

    modificacion en tranferirSocio

--- resources/views/usuarios/transferirSocios.blade.php
+++ resources/views/usuarios/transferirSocios.blade.php
@@ -4,11 +4,29 @@
 
 @include('mensajes.alertas')
 
-<button class="btn btn-success btn-md mt-3 fa fa-search system-icons mb-2" data-toggle="modal" data-target="#filtrar_users"> Realizar una búsqueda</button>
-@include('socios.filtrarEjecutivo')
+@if(count($socios))
+  <div class="card text-center mt-2">
+  <div class="card-header text-primary">
+    Filtro de Busqueda
+  </div>
+
+  <div class="card-block">
+
+  <select id="select" class="custom-select mb-2 mb-sm-0" name="Criterio">
+    <option selected value="0">N° de socio</option>
+    <option value="1">Cédula</option>
+    <option value="2">Nombre</option>
+  </select>
+
+  <label class="custom-control custom-checkbox mb-2 mb-sm-0" style="margin-left: 1.8%;">
+        <input id="valor" type="text" class="form-control" placeholder="Buscar" type="text" name="valor" value="{{ old('valor') }}" onkeyup="filtrar()" required autofocus>
+  </label>
+</div>
+</div>
+@endif
 
 <!--_______________________________ Tabla _____________________________-->
-<form method="GET" action="/usuarios/seleccionarNuevoEjecutivo" >
+<form name="form_liquidar_cobro" method="GET" action="/usuarios/seleccionarNuevoEjecutivo" >
   {{ csrf_field() }}
 <div class="card text-center ">
 
@@ -22,16 +40,25 @@
              <label class="text-primary"> Socios Activos del Usuario " {{ $usuario->nombre_usuario }} "</label>      
       </div>
 
-       <div class="col-md-5">
+       <div class="col-md-5 float-right">
            <div class="nav-item float-md-right float-xs-center">
        <button type="submit" class="btn btn-success btn-xs fa fa-check system-icons" style="color: white;">Siguiente</button>
          <a href="/usuarios/home" class="btn btn-danger fa fa-times system-icons">
         <span class="glyphicon glyphicon-remove-circle "></span> Cancelar </a>
-    </div>  
+    </div> 
+        @if(count($socios))
+   <ul class="float-right mr-3 mt-1">
+    <li class="list-unstyled dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Opciones</a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="javascript:seleccionar_todo()">Marcar todo</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="javascript:deseleccionar_todo()">Desmarcar todo</a>
+        </div>
+    </li>
+  </ul>  
+  @endif 
       </div>
-  
-    
-    
       </div>
          
   </div>
@@ -41,7 +68,7 @@
     <div class="container col-md-8">
 <div class="row">
 
- <div class="table-responsive  table-condensed">
+ <div class="table-responsive  table-condensed scroll">
         
     <table id="table" WIDTH="100%" class="table table-responsive" >
         <thead>
@@ -63,7 +90,7 @@
             <td class="info" > {{ $socio->persona->primer_nombre }} {{ $socio->persona->primer_apellido }} {{ $socio->persona->segundo_apellido }} </td>
    
             <td class="info"> 
-              <input type="radio" name="radio" value="{{$socio->id}}">
+              <input type="checkbox" value="{{$socio->id}}" name="{{$socio->id}}">
             </td>
         </tr>
 
